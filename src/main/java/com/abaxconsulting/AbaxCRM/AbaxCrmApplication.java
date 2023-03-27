b076ca1047cb15d6a8c5f573660e31dbc6896601
ecarballea <84036372+ecarballea@users.noreply.github.com>
2023-03-27 12:17:40 -0400

    First Commit. Init Abax CRM Project

---
+++ src/main/java/com/abaxconsulting/AbaxCRM/AbaxCrmApplication.java
@@ -0,0 +1,13 @@
+package com.abaxconsulting.AbaxCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbaxCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbaxCrmApplication.class, args);
+	}
+
+}
